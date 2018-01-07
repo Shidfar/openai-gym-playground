2b2c3128ec8e28f86ac87c07c4d405bf3f73f27d
Shidfar Hodizoda <shidfarhf@yahoo.com>
2018-01-07 23:29:24 +0800

    solved with hill-climbing

--- playgound/cartpole.py
+++ playgound/cartpole.py
@@ -8,16 +8,16 @@ def run_episode(env, parameters):
     observation = env.reset()
     env.render()
     # sleep(5)
-    totalreward = 0
-    for t in range(200):
+    total_reward = 0
+    for _ in range(200):
         action = 0 if numpy.matmul(parameters, observation) < 0 else 1
         observation, reward, done, info = env.step(action)
         # print(" [ observation ] ", observation)
-        totalreward += reward
+        total_reward += reward
         if done:
-            print("Episode finished after {} timesteps".format(t + 1))
+            # print("Episode finished after {} timesteps".format(t + 1))
             break
-    return totalreward
+    return total_reward
 
 def manual_control(env):
     for t in range(200):
@@ -59,16 +59,34 @@ def random_search(env):
 def hill_climbing(env):
     noise_scaling = 0.1
     parameters = numpy.random.rand(4) * 2 - 1
-    bestreward = 0
+    best_reward = 0
+    p_run = 0
+    stop: bool = False
+    episodes_per_update = 10
     for _ in range(10000):
-        newparams = parameters + (numpy.random.rand(4) * 2 - 1) * noise_scaling
+        new_params = parameters + (numpy.random.rand(4) * 2 - 1) * noise_scaling
         reward = 0
-        run = run_episode(env, newparams)
-        if reward > bestreward:
-            bestreward = reward
-            parameters = newparams
-            if reward == 200:
+        for _ in range(episodes_per_update):
+            run = run_episode(env, new_params)
+            if p_run == run:
+                noise_scaling += 0.01
+            if p_run > run:
+                noise_scaling -= 0.01
+            print("[ episode reward ]", run)
+            reward += run
+            if (run == 200):
+                stop = True
                 break
+        print("[ total reward ] ", reward)
+        if reward > best_reward or stop:
+            best_reward = reward
+            parameters = new_params
+            if stop:
+                break
+        if reward == best_reward:
+            noise_scaling -= 0.01
+        else :
+            noise_scaling += 0.01
 
 
 env = gym.make('CartPole-v0')
